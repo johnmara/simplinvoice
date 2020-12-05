ea56551babfbaae9934af6f5d9a2031d5c445bfe
John Mara <johnmara94@gmail.com>
2020-12-06 01:08:43 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/invoicesmanagement/InvoicesManagementApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.invoicesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/invoicesmanagement/InvoicesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.invoicesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
